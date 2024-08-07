d48141c3374799d2a2d2bd549c730ee152321099
yzykzy <yzykzy@users.noreply.github.com>
2024-08-07 08:56:44 +0000

    deploy: f6c2c09740f23b0b3fccd4dc7cb67cb3bf3e77e2

--- assets/app.OaphQXg5.js
+++
@@ -1,7 +0,0 @@
-import{y as s,ad as p,ae as u,af as l,ag as c,ah as f,ai as d,aj as m,ak as h,al as A,am as g,Z as v,d as P,q as y,o as w,D as C,an as R,ao as _,ap as D,a5 as E}from"./chunks/framework.CCN56uzi.js";import{R as b}from"./chunks/theme.LCAFZ1za.js";function i(e){if(e.extends){const a=i(e.extends);return{...a,...e,async enhanceApp(t){a.enhanceApp&&await a.enhanceApp(t),e.enhanceApp&&await e.enhanceApp(t)}}}return e}const o=i(b),T=P({name:"VitePressApp",setup(){const{site:e,lang:a,dir:t}=y();return w(()=>{C(()=>{document.documentElement.lang=a.value,document.documentElement.dir=t.value})}),e.value.router.prefetchLinks&&R(),_(),D(),o.setup&&o.setup(),()=>E(o.Layout)}});async function j(){const e=O(),a=L();a.provide(u,e);const t=l(e.route);return a.provide(c,t),a.component("Content",f),a.component("ClientOnly",d),Object.defineProperties(a.config.globalProperties,{$frontmatter:{get(){return t.frontmatter.value}},$params:{get(){return t.page.value.params}}}),o.enhanceApp&&await o.enhanceApp({app:a,router:e,siteData:m}),{app:a,router:e,data:t}}function L(){return h(T)}function O(){let e=s,a;return A(t=>{let n=g(t),r=null;return n&&(e&&(a=n),(e||a===n)&&(n=n.replace(/\.js$/,".lean.js")),r=v(()=>import(n),__vite__mapDeps([]))),s&&(e=!1),r},o.NotFound)}s&&j().then(({app:e,router:a,data:t})=>{a.go().then(()=>{p(a.route,t.site),e.mount("#app")})});export{j as createApp};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
